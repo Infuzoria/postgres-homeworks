47569be3b035785b2c77f62bbd7f2010a09b25f4
shurochka <alexa.sazhaeva@gmail.com>
2023-08-29 22:50:14 +0200

    Fixed filter_sorting.sql

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -1,6 +1,7 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
-SELECT order_id, (shipped_date - order_date) as difference FROM orders;
+SELECT * FROM orders WHERE ship_country
+IN ('France', 'Germany', 'Spain');
 
 -- 2. уникальные страны и города, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
 SELECT DISTINCT ship_country, ship_city FROM orders ORDER BY ship_country, ship_city;
